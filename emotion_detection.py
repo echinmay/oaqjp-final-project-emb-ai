6a2ebaef59f1bc9a8f42d0880a65e088931b99f6
Chinmay Padhye <echinmay@gmail.com>
2025-02-15 12:15:34 -0500

    ...

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,6 +1,16 @@
 import requests
 import json
 
+def process_response(rsp_j):
+    result = {} # The return dict after parsing the response
+    emotion_d = {} # Dict containing the emotion values
+    for d in rsp_j['emotionPredictions']:
+        if 'emotion' in d:
+            emotion_d = d
+            break
+    allemotions = ['']
+    
+
 def emotion_detector(text_to_analyze):
     URL = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     Headers =  {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
@@ -9,6 +19,8 @@ def emotion_detector(text_to_analyze):
     try:
         response = requests.post(URL, headers=Headers, json=Input)
         response.raise_for_status()  # Raise an error for bad HTTP responses
+        rsp_j = response.json()
+
         return response.text
     except requests.exceptions.RequestException as e:
         return {"error": str(e)}
