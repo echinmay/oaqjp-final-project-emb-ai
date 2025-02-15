4224e5ed2bea03eea691562cc6f410fc8e5a3258
Chinmay Padhye <echinmay@gmail.com>
2025-02-15 12:22:52 -0500

    ...

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,14 +1,5 @@
 import requests
 import json
-
-def process_response(rsp_j):
-    result = {} # The return dict after parsing the response
-    emotion_d = {} # Dict containing the emotion values
-    for d in rsp_j['emotionPredictions']:
-        if 'emotion' in d:
-            emotion_d = d
-            break
-    allemotions = ['']
     
 
 def emotion_detector(text_to_analyze):
@@ -19,8 +10,6 @@ def emotion_detector(text_to_analyze):
     try:
         response = requests.post(URL, headers=Headers, json=Input)
         response.raise_for_status()  # Raise an error for bad HTTP responses
-        rsp_j = response.json()
-
         return response.text
     except requests.exceptions.RequestException as e:
         return {"error": str(e)}
